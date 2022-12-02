65f147a3db8e62b37ff3bc304f420a6be1bf1d8d
ObamaTheLlama114 <william.chomel@gmail.com>
2022-12-01 19:04:56 -0600

    changed deserialization method in web server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,11 @@
-use core::init_auth;
 use std::{
+    collections::HashMap,
     env,
     process::exit,
     sync::{Arc, Mutex},
 };
-
+use core::*;
+use dotenv::dotenv;
 use actix_web::{
     delete, get, patch, post,
     web::{self, Data},
@@ -64,195 +65,201 @@ async fn main() {
     .unwrap();
 }
 
-// Structs for the handlers to represent JSON data in the request body
-mod data_structs {
-    use serde::Deserialize;
-
-    #[derive(Deserialize)]
-    pub(crate) struct LoginData {
-        pub email: String,
-        pub password: String,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct LogoutData {
-        pub token: String,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct TokenVerifyData {
-        pub token: String,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct RegisterData {
-        pub email: String,
-        pub password: String,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct UpdateUserData {
-        pub token: String,
-        pub email: Option<String>,
-        pub password: Option<String>,
-        pub logout: Option<bool>,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct AdminUpdateUserData {
-        pub filter: String,
-        pub email: Option<String>,
-        pub password: Option<String>,
-        pub logout: Option<bool>,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct DeleteUserData {
-        pub token: String,
-    }
-
-    #[derive(Deserialize)]
-    pub(crate) struct AdminDeleteUserData {
-        pub filter: String,
-    }
-}
-use data_structs::*;
-
 // Handlers for the web server
-mod handlers {
-    use core::*;
-    use std::collections::HashMap;
 
-    use crate::*;
-
-    #[post("/login")]
-    pub(crate) async fn login_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        login_data: web::Json<LoginData>,
-    ) -> HttpResponse {
-        let login_data = login_data.into_inner();
-        let mut auth = auth_data.lock().unwrap();
-        match login(&mut auth, login_data.email, login_data.password).await {
-            Ok(x) => HttpResponse::Ok().body(x),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
-    }
-
-    #[post("/logout")]
-    pub(crate) async fn logout_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        logout_data: web::Json<LogoutData>,
-    ) -> HttpResponse {
-        let mut auth = auth_data.lock().unwrap();
-        let logout_data = logout_data.into_inner();
-        match logout(&mut auth, logout_data.token) {
-            Ok(_) => HttpResponse::Ok().body(""),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
-    }
-
-    #[get("/token")]
-    pub(crate) async fn token_verify_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        token_data: web::Json<TokenVerifyData>,
-    ) -> HttpResponse {
-        let mut auth = auth_data.lock().unwrap();
-        let token_data = token_data.into_inner();
-        match verify_token(&mut auth, token_data.token).await {
-            Ok(x) => {
-                if x != "" {
-                    HttpResponse::Ok().json(HashMap::from([("email", x)]))
-                } else {
-                    HttpResponse::BadRequest()
-                        .json(HashMap::from([("error", "Invalid token".to_string())]))
-                }
-            }
-            Err(x) => {
-                HttpResponse::BadRequest().json(HashMap::from([("error", x.to_error_message())]))
-            }
-        }
-    }
-
-    #[post("/user")]
-    pub(crate) async fn register_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        register_data: web::Json<RegisterData>,
-    ) -> HttpResponse {
-        let register_data = register_data.into_inner();
-        let mut auth = auth_data.lock().unwrap();
-        match create_user(&mut auth, register_data.email, register_data.password).await {
-            Ok(_) => HttpResponse::Ok().body(""),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
-    }
-
-    #[patch("/user")]
-    pub(crate) async fn update_user_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        update_data: web::Json<UpdateUserData>,
-    ) -> HttpResponse {
-        let update_data = update_data.into_inner();
-        let mut auth = auth_data.lock().unwrap();
-        match update_user(
-            &mut auth,
-            update_data.token,
-            update_data.email,
-            update_data.password,
-            update_data.logout.unwrap_or(false),
-        )
-        .await
-        {
-            Ok(_) => HttpResponse::Ok().body(""),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
-    }
-
-    #[delete("/user")]
-    pub(crate) async fn delete_user_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        delete_data: web::Json<DeleteUserData>,
-    ) -> HttpResponse {
-        let delete_data = delete_data.into_inner();
-        let mut auth = auth_data.lock().unwrap();
-        match delete_user(&mut auth, delete_data.token).await {
-            Ok(_) => HttpResponse::Ok().body(""),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
-    }
-
-    #[patch("/admin/user")]
-    pub(crate) async fn admin_update_user_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        update_data: web::Json<AdminUpdateUserData>,
-    ) -> HttpResponse {
-        let update_data = update_data.into_inner();
-        let mut auth = auth_data.lock().unwrap();
-        match admin_update_user(
-            &mut auth,
-            update_data.filter,
-            update_data.email,
-            update_data.password,
-            update_data.logout.unwrap_or(false),
-        )
-        .await
-        {
-            Ok(_) => HttpResponse::Ok().body(""),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
-    }
-
-    #[delete("/admin/user")]
-    pub(crate) async fn admin_delete_user_handler(
-        auth_data: Data<Arc<Mutex<Auth>>>,
-        delete_data: web::Json<AdminDeleteUserData>,
-    ) -> HttpResponse {
-        let delete_data = delete_data.into_inner();
-        let mut auth = auth_data.lock().unwrap();
-        match admin_delete_user(&mut auth, delete_data.filter).await {
-            Ok(_) => HttpResponse::Ok().body(""),
-            Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
-        }
+#[post("/login")]
+pub(crate) async fn login_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    login_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let login_data = login_data.into_inner();
+    let email = match login_data["email"] {
+        serde_json::Value::String(ref email) => email,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let password = match login_data["password"] {
+        serde_json::Value::String(ref password) => password,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match login(&mut auth, email.clone(), password.clone()).await {
+        Ok(x) => HttpResponse::Ok().body(x),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
     }
 }
-use dotenv::dotenv;
-use handlers::*;
+
+#[post("/logout")]
+pub(crate) async fn logout_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    logout_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let logout_data = logout_data.into_inner();
+    let token = match logout_data["token"] {
+        serde_json::Value::String(ref token) => token,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match logout(&mut auth, token.clone()) {
+        Ok(_) => HttpResponse::Ok().body(""),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
+    }
+}
+
+#[get("/token")]
+pub(crate) async fn token_verify_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    token_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let token_data = token_data.into_inner();
+    let token = match token_data["token"] {
+        serde_json::Value::String(ref token) => token,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match verify_token(&mut auth, token.clone()).await {
+        Ok(x) => {
+            if x != "" {
+                HttpResponse::Ok().json(HashMap::from([("email", x)]))
+            } else {
+                HttpResponse::BadRequest()
+                    .json(HashMap::from([("error", "Invalid token".to_string())]))
+            }
+        }
+        Err(x) => HttpResponse::BadRequest().json(HashMap::from([("error", x.to_error_message())])),
+    }
+}
+
+#[post("/user")]
+pub(crate) async fn register_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    register_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let register_data = register_data.into_inner();
+    let email = match register_data["email"] {
+        serde_json::Value::String(ref email) => email,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let password = match register_data["password"] {
+        serde_json::Value::String(ref password) => password,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match create_user(&mut auth, email.clone(), password.clone()).await {
+        Ok(_) => HttpResponse::Ok().body(""),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
+    }
+}
+
+#[patch("/user")]
+pub(crate) async fn update_user_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    update_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let update_data = update_data.into_inner();
+    let token = match update_data["token"] {
+        serde_json::Value::String(ref token) => token,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let email = match update_data["email"] {
+        serde_json::Value::String(ref email) => Some(email.clone()),
+        serde_json::Value::Null => None,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let password = match update_data["password"] {
+        serde_json::Value::String(ref password) => Some(password.clone()),
+        serde_json::Value::Null => None,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let logout = match update_data["logout"] {
+        serde_json::Value::Bool(ref logout) => Some(*logout),
+        serde_json::Value::Null => None,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match update_user(
+        &mut auth,
+        token.clone(),
+        email,
+        password,
+        logout.unwrap_or(false),
+    )
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().body(""),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
+    }
+}
+
+#[delete("/user")]
+pub(crate) async fn delete_user_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    delete_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let delete_data = delete_data.into_inner();
+    let token = match delete_data["token"] {
+        serde_json::Value::String(ref token) => token,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match delete_user(&mut auth, token.clone()).await {
+        Ok(_) => HttpResponse::Ok().body(""),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
+    }
+}
+
+#[patch("/admin/user")]
+pub(crate) async fn admin_update_user_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    update_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let update_data = update_data.into_inner();
+    let filter = match update_data["filter"] {
+        serde_json::Value::String(ref token) => token,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let email = match update_data["email"] {
+        serde_json::Value::String(ref email) => Some(email.clone()),
+        serde_json::Value::Null => None,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let password = match update_data["password"] {
+        serde_json::Value::String(ref password) => Some(password.clone()),
+        serde_json::Value::Null => None,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let logout = match update_data["logout"] {
+        serde_json::Value::Bool(ref logout) => Some(*logout),
+        serde_json::Value::Null => None,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match admin_update_user(
+        &mut auth,
+        filter.clone(),
+        email,
+        password,
+        logout.unwrap_or(false),
+    )
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().body(""),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
+    }
+}
+
+#[delete("/admin/user")]
+pub(crate) async fn admin_delete_user_handler(
+    auth_data: Data<Arc<Mutex<Auth>>>,
+    delete_data: web::Json<HashMap<String, serde_json::Value>>,
+) -> HttpResponse {
+    let delete_data = delete_data.into_inner();
+    let filter = match delete_data["filter"] {
+        serde_json::Value::String(ref token) => token,
+        _ => return HttpResponse::BadRequest().finish(),
+    };
+    let mut auth = auth_data.lock().unwrap();
+    match admin_delete_user(&mut auth, filter.clone()).await {
+        Ok(_) => HttpResponse::Ok().body(""),
+        Err(x) => HttpResponse::BadRequest().body(x.to_error_message()),
+    }
+}
